ec70f1c94197e8106fde47c25f26ec81015fa042
Moumouuu <robin.travail007@gmail.com>
2024-03-20 09:42:51 +0100

    feat: reach 67%

--- main.py
+++ main.py
@@ -60,11 +60,11 @@ def add_image_to_list():
             image = convert_grayscale_to_black_and_white(image)
 
             # show_image(image)
-            list_of_images.append({i: image})
+            list_of_images.append((i, image))
     return list_of_images
 
 
-def create_zone(image):
+def create_zone(image: np.ndarray):
     """
     Divise l'image en 9 zones et calcule la somme de chaque zone de pixels
       noirs
@@ -73,9 +73,11 @@ def create_zone(image):
     """
     zones = []
 
+    # image have dict values ??
+
     height, width = image.shape[:2]
-    zone_height = height // 3
-    zone_width = width // 3
+    zone_height = height // 7
+    zone_width = width // 5
 
     for i in range(0, height, zone_height):
         for j in range(0, width, zone_width):
@@ -99,25 +101,30 @@ def compare_images(unknown_image, known_image1: dict, known_image2: dict):
     :return: The class of the closest image
     """
 
-    distance_from_image1 = calculate_distance(unknown_image, known_image1[0])
-    distance_from_image2 = calculate_distance(unknown_image, known_image2[0])
+    distance_from_image1 = calculate_distance(
+        unknown_image, known_image1[1])
+    distance_from_image2 = calculate_distance(
+        unknown_image, known_image2[1])
 
-    print("image 1 " + str(distance_from_image1))
-    print("image 2 " + str(distance_from_image2))
+    print("image 1 " + str(distance_from_image1) +
+          " class :" + str(known_image1[0]))
+    print("image 2 " + str(distance_from_image2) +
+          " class :" + str(known_image2[0]))
 
     if distance_from_image1 < distance_from_image2:
-        return known_image1.keys()
+        return known_image1[0]
     else:
-        return known_image2.keys()
+        return known_image2[0]
 
 
 def calculate_distance(unknown_image, known_image):
     """
     Calculate the distance between two images
-    :param unknown_image: The unknown image
-    :param known_image: The known image
+    :param unknown_image: The unknown image (numpy array)
+    :param known_image: The known image (numpy array)
     :return: The distance (float)
     """
+
     unknown_image_zones = create_zone(unknown_image)
     known_image_zones = create_zone(known_image)
 
@@ -130,16 +137,62 @@ def calculate_distance(unknown_image, known_image):
 
 
 def main():
-    # todo : bug avec les index des list of image
     list_of_images = add_image_to_list()
 
-    print(len(list_of_images))
+    correct_predictions = 0
+    total_tests = 0
 
-    print(list_of_images[1])
-    print(list_of_images[12])
+    for unknown_image_index in range(len(list_of_images)):
+        unknown_image = list_of_images[unknown_image_index][1]
+        true_class = list_of_images[unknown_image_index][0]
 
-    print(compare_images(list_of_images[0][0],
-          list_of_images[1], list_of_images[12]))
+        closest_class = None
+        min_distance = float('inf')
+
+        for i in range(len(list_of_images)):
+            if i != unknown_image_index:
+                known_image_class = list_of_images[i][0]
+                known_image = list_of_images[i][1]
+
+                distance = calculate_distance(unknown_image, known_image)
+
+                if distance < min_distance:
+                    min_distance = distance
+                    closest_class = known_image_class
+
+        total_tests += 1
+        if closest_class == true_class:
+            correct_predictions += 1
+
+    success_rate = (correct_predictions / total_tests) * 100
+
+    print("Success rate: {:.2f}%".format(success_rate))
+
+    # Tableau récapitulatif
+    print("Classified Image | True Image")
+    for i in range(len(list_of_images)):
+        unknown_image_class = list_of_images[i][0]
+        predicted_image_class = None
+        min_distance = float('inf')
+
+        for j in range(len(list_of_images)):
+            if j != i:
+                known_image_class = list_of_images[j][0]
+                known_image = list_of_images[j][1]
+
+                distance = calculate_distance(
+                    list_of_images[i][1], known_image)
+
+                if distance < min_distance:
+                    min_distance = distance
+                    predicted_image_class = known_image_class
+
+        print("{:<16} | {:<11}".format(
+            predicted_image_class, unknown_image_class))
 
 
 main()
+
+# Retour : dire les caractéristique choisis +image de la matrice
+# et à la fin un classifieur+ résultat obtenue et dire le
+# taux d'apprentissage sur une image trouvé
